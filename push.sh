d9a6bb22e616f8875782f81e290f2c4d95f93972
Vijesh Kirodian <vkirodia@cisco.com>
2018-08-22 14:30:23 +0530

    Updated

--- push.sh
+++ push.sh
@@ -9,16 +9,15 @@ setup_git() {
 
 commit_website_files() {
   echo "Checkout and Commit Start"
-  git checkout -b develop
-  git add . /tmp/lib/*.jar
-  git commit --message "Travis build"
+  git checkout -b dev
+  git commit -am "Travis build"
   echo "Checkout and Commit End"
 }
 
 upload_files() {
   echo "Push Start"
-  git remote add origin-pages https://${GIT_TOKEN}@github.com/vkirodian/library-management-system.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages develop 
+  git remote add inventory_issue https://${GIT_TOKEN}@github.com/vkirodian/library-management-system.git > /dev/null 2>&1
+  git push --quiet --set-upstream inventory_issue dev 
   echo "Push End"
 }
 
