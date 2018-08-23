14e22ca1db92fb6444601adebf03cab60983ed2b
Vijesh Kirodian <vkirodia@cisco.com>
2018-08-23 18:53:44 +0530

    Test

--- push.sh
+++ push.sh
@@ -4,14 +4,14 @@ setup_git() {
   echo "Configuration Start"
   git config --global user.email ${EMAIL}
   git config --global user.name ${USERNAME}
-  echo $TRAVIS_PULL_REQUEST_BRANCH
+  echo $1
 #  git remote set-url origin https://${GIT_TOKEN}@github.com/vkirodian/library-management-system.git
   echo "Configuration End"
 }
 
 commit_files() {
   echo "Commit Start"
-  git checkout $TRAVIS_PULL_REQUEST_BRANCH
+  git checkout $1
   git commit -am "Travis build $TRAVIS_BUILD_NUMBER"
   echo "Commit End"
 }
@@ -19,7 +19,7 @@ commit_files() {
 upload_files() {
   echo "Push Start"
   git remote add develop https://${GIT_TOKEN}@github.com/vkirodian/library-management-system.git > /dev/null 2>&1
-  git push --quiet --set-upstream develop  $TRAVIS_PULL_REQUEST_BRANCH 
+  git push --quiet --set-upstream develop  $1 
   echo "Push End"
 }
 
